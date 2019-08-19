dd8b48483626d852d07546b065ede535d279a04a
watila <watila.costa@fortaleza.ce.gov.br>
2019-08-19 14:48:45 -0300

    Criando o componente agenda / add dependencia react-native-vector-icons

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import Agenda from './src/telas/Agenda'
+
+AppRegistry.registerComponent(appName, () => Agenda);
